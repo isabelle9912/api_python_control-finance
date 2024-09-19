26fd8fb52ab2f42916011f5587531c090037dbe9
isabelle9912 <isabellesaahirah28@gmail.com>
2024-09-19 15:04:03 -0300

    feat: gerando grafico de cada mes por categoria

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import io
 import base64
 import traceback
@@ -54,6 +55,48 @@ def gerar_grafico():
         print(f"Erro: {e}")
         print(traceback.format_exc())
         return jsonify({"error": "Erro interno do servidor"}), 500
+
+
+@app.route('/GerarGraficoCategorias', methods=['POST'])
+def gerar_teste():
+    # Obtendo os dados enviados pela api
+    data = request.json
+    # Criando um DataFrame
+    df = pd.DataFrame(data)
+
+    # Filtra os dados e calcula os totais
+    total_saida_pessoal = df[(df['type'] == 'Saida') & (df['category'] == 'Pessoal')]['value'].sum()
+    total_entrada_pessoal = df[(df['type'] == 'Entrada') & (df['category'] == 'Pessoal')]['value'].sum()
+    total_saida_profissional = df[(df['type'] == 'Saida') & (df['category'] == 'Profissional')]['value'].sum()
+    total_entrada_profissional = df[(df['type'] == 'Entrada') & (df['category'] == 'Profissional')]['value'].sum()
+
+    # Exibe os resultados
+    print(f"Total Saída Pessoal: {total_saida_pessoal}")
+    print(f"Total Entrada Pessoal: {total_entrada_pessoal}")
+    print(f"Total Saída Profissional: {total_saida_profissional}")
+    print(f"Total Entrada Profissional: {total_entrada_profissional}")
+
+    # Prepara os dados para o gráfico
+    labels = ['S Pessoal', 'E Pessoal', 'S Profissional', 'E Profissional']
+    values = [total_saida_pessoal, total_entrada_pessoal, total_saida_profissional, total_entrada_profissional]
+
+    # Gera o gráfico de barras
+    plt.bar(labels, values)
+    plt.xlabel('Categoria')
+    plt.ylabel('Total (R$)')
+    plt.title('Totais por Categoria e Tipo')
+    
+    # Salvar o gráfico em um objeto de memória
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.close()
+    
+    # Codifica a imagem em base64
+    img_base64 = base64.b64encode(img.read()).decode('utf-8')
+
+    # Retornar a imagem como uma string base64
+    return jsonify({"image": img_base64})
 
 
 if __name__ == "__main__":
